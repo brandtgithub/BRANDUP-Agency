86d84780524fda191aca0b6fec3a73591343dbd3
brandt <angelita.diias97@gmail.com>
2024-10-28 11:28:06 -0300

    Add files via upload

---
+++ oficial.js
@@ -0,0 +1,13 @@
+// Código do Menu responsivo mobile
+function mostrarmenu() {
+    // declaração da variável para manipulação
+    
+    let menu = document.getElementById('menu-mobile');
+    // laço de condição para ajustar o menu
+    if (menu.style.transform === 'scale(0)') {
+        menu.style.transform = 'scale(1)';
+    } else {
+        menu.style.transform = 'scale(0)';
+    }
+    
+}
